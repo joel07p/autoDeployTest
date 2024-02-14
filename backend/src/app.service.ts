25502c5ed7030659f028fbaec7f2cb5000537d70
User <joelpalea@gmail.com>
2024-02-14 08:40:05 +0100

    test remote deployment

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!121313123';
+    return 'Hello World!';
   }
 }
