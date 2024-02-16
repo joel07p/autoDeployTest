2b81889cf9b2fad68ea7dead4fa586ddd3d56f02
tobiasmi <mikatobi12@gmail.com>
2024-02-16 09:45:12 +0100

    ip added

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Backend!';
+  getHello(ip: string): string {
+    return `Hello ${ip}!`;
   }
 }
